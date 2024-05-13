b6bba11a6afac03adc1731ac203725941e9f3b41
cread134 <96459595+cread134@users.noreply.github.com>
2024-05-13 19:55:15 +1000

    creationc lass

--- Assets/Scripts/Core.Service.Editor/AudioManager/AudioObjectCreatorTool.cs
+++ Assets/Scripts/Core.Service.Editor/AudioManager/AudioObjectCreatorTool.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
-public class AudioObjectCreatorTool : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+namespace Core.Service.AudioManagement {
+    public class AudioObjectCreatorTool
     {
-        
-    }
+        [MenuItem("Assets/Create/GameSound_Selected", priority = 1)]
+        public static void CreateGameSound()
+        {
+            var clips = GetSelectedClips();
+            if (clips.Count == 0)
+            {
+                return;
+            }
+            var createdInstance = ScriptableObject.CreateInstance<GameSound>();
+            var path = AssetDatabase.GetAssetPath(clips.First());
+            var name = Path.GetFileName(path);
+            var directory = Path.GetDirectoryName(path);
+            var usePath = Path.Combine(directory, $"{name}_object");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            AssetDatabase.CreateAsset(createdInstance, usePath);
+
+            if (clips.Count > 0)
+            {
+                createdInstance.AudioClips = clips.ToArray();
+            }
+
+            EditorUtility.SetDirty(createdInstance);
+            AssetDatabase.SaveAssets();
+            Selection.activeObject = createdInstance;
+            EditorUtility.FocusProjectWindow();
+        }
+
+        public static List<AudioClip> GetSelectedClips()
+        {
+            var selected = Selection.GetFiltered(typeof(AudioClip), SelectionMode.Assets);
+            return selected.Cast<AudioClip>().ToList();
+        }
     }
 }
